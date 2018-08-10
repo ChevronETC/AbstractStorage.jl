d97c917e7ff57323c2dbdfe18006f0bf10b2feca
samtkaplan <sam.kaplan@chevron.com>
2018-08-10 09:41:19 -0500

    updates for julia 0.7

--- src/AbstractStorage.jl
+++ src/AbstractStorage.jl
@@ -1,13 +1,10 @@
-__precompile__(true)
-
 module AbstractStorage
 
 abstract type Container end
 
-function readpieces() end
 function readpieces!() end
 function writepieces() end
 
-export Container, readpieces, readpieces!, writepieces
+export Container, readpieces!, writepieces
 
 end
