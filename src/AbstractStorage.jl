5acbf1e42fcd06dc2594680f35b24df383b297dc
Sam Kaplan <Sam.Kaplan@chevron.com>
2020-08-11 22:55:18 +0000

    non mutating scrubsession

--- src/AbstractStorage.jl
+++ src/AbstractStorage.jl
@@ -4,8 +4,9 @@ abstract type Container end
 
 Container(::Type{C}, d::Dict) where {C<:Container} = nothing
 session(container::C) where {C<:Container} = nothing
+scrubsession(container::C) where {C<:Container} = copy(container)
 scrubsession!(container::C) where {C<:Container} = container
 
-export Container, scrubsession!, session
+export Container, scrubsession, scrubsession!, session
 
 end
